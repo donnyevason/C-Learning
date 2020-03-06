bd5d534605e8837d7ba33de58e1c347c699cb3d7
donnyevason <l1103774@live.tees.ac.uk>
2020-03-06 12:50:52 +0000

    Add files via upload

---
+++ Lesson4/TrueAndFalse.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Lesson4
+{
+    class TrueAndFalse
+    {
+        static void Main(string[] args)
+        {
+            bool trueValue = true;
+            Console.WriteLine("This is a true value:\t" + trueValue);
+
+            bool falseValue = false;
+            Console.WriteLine("This is a false value:\t" + falseValue);
+
+            /* Comparison Operators
+             *  ==, Is Equal To
+             * !=, Is Not Equal TO
+             * >, greater than
+             * <. less than
+             * >=, greater than or equal to
+             * <=, less than or equal to
+             */
+
+            bool value1 = 7 == 7;
+            Console.WriteLine("7 IS EQUAL TO 7:\t" + value1);
+
+            bool value2 = 7 != 7;
+            Console.WriteLine("7 IS NOT EQUAL TO 7:\t" + value2);
+
+            bool value3 = 99 > 1;
+            Console.WriteLine("99 IS GREATER THAN 1:\t" + value3);
+
+            bool value4 = 99 < 1;
+            Console.WriteLine("99 IS LESS THAN 1:\t" + value4);
+
+            bool value5 = 2 >= 1;
+            Console.WriteLine("2 IS GREATER THAN OR EQUAL TO 1:\t" + value5);
+
+            bool value6 = 2 <= 2;
+            Console.WriteLine("2 IS LESS THAN OR EQUAL TO 2:\t" + value6);
+
+            bool value7 = "cup" == "CUP";
+            Console.WriteLine("cup IS EQUAL TO CUP:\t" + value7);
+
+
+
+        }
+    }
+}
---
+++ Lesson4Task/TrueAndFalseTask.cs
@@ -0,0 +1,45 @@
+﻿
+/*
+* TASK:
+* Below, you will see comparison statements written in english being displayed by Console.WriteLine.
+* Make a bool variable that executes the comparison described in the Console.WriteLine.
+* Then concatinate the bool to the end of the string.
+*/
+
+using System;
+
+namespace Lesson4Task
+{
+    class TrueAndFalseTask
+    {
+        static void Main(string[] args)
+        {
+            bool value1 = 7 > 9;
+            Console.WriteLine("7 is greater than 9: " + value1);
+
+            bool value2 = 12 > 15;
+            Console.WriteLine("12 is less than 15: " + value2);
+
+            bool value3 = 91 >= 19;
+            Console.WriteLine("91 is greater than or equal to 19: " + value3);
+
+            bool value4 = 1.2 != 6.6;
+            Console.WriteLine("1.2 is not equal to 6.6: " + value4);
+
+            bool value5 = 17.9 <= 17.91;
+            Console.WriteLine("17.9 is less than or equal to 17.91: " + value5);
+
+            bool value6 = 7.5 > 7.4;
+            Console.WriteLine("7.5 is greater than 7.4: " + value6);
+
+            bool value7 = "ball" == "BALL";
+            Console.WriteLine("ball equals BALL: " + value7);
+
+            bool value8 = "cup" == "cup";
+            Console.WriteLine("cup equals cup: " + value8);
+
+            bool value9 = "pan" != "Pan";
+            Console.WriteLine("pan is not equal to Pan: " + value9);
+        }
+    }
+}
