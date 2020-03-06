76a626cff2d6152458a5c11cbaafc571bcfe0127
donnyevason <l1103774@live.tees.ac.uk>
2020-03-06 12:53:22 +0000

    Add files via upload

---
+++ Lesson3/DoingMath.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lesson3
+{
+    class Lesson3
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter an Int!: ");
+            //int x = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter another Int!: ");
+            //int y = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("X: " + x + ", y: " + y);
+
+            //Console.WriteLine("X * y = " + x * y );
+            //Console.WriteLine("X + Y = " + (x + y)); //(if X = 4 and y = 2 this will concatonate to give 42 | "X + Y + X" then adds Y to the end (4 + 2 + 2)
+            //So we add " ( ) " to do addition first
+
+            //Console.WriteLine("x / y = " + (x / y));
+            //Console.WriteLine("y / x - " + (y / (double) x));
+
+            Console.Write("Enter a Double:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter another Double: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("a: " + a + ", b: " + b);
+
+            Console.WriteLine(" a / b = " + (a / b));
+            Console.WriteLine(" a + b = " + (a + b));
+
+            double addResult = a + b;
+            double roundedResult = Math.Round(addResult, 1);
+            Console.WriteLine("Rounded: a + b = " + roundedResult);
+        }
+    }
+}
---
+++ lesson3Task/DoingMathTask.cs
@@ -0,0 +1,53 @@
+﻿/*
+* TASK:
+* Create a program that calculates an employees' pay.
+* Ask for their Employee ID. This value will always be an INT.
+* Ask for the number of hours they worked.
+* Ask for their hourly pay rate.
+* 
+* Example Output
+*      Enter employee ID: 1154856
+*      Enter hours worked: 16.55
+*      Enter hourly pay: 12.77
+*      Employee 1154856, you have earned $211.34‬
+* 
+*/
+
+using System;
+
+namespace Lesson3Task
+{
+    class DoingMathTask
+    {
+        static void Main(string[] args)
+        {
+            //NOTE: You will need to use convert.
+
+            //Step 1: Ask for the employee ID. Store the input from the user into an int variable.
+
+            Console.WriteLine("Enter your ID number: ");
+            int ID = Convert.ToInt32(Console.ReadLine());
+
+            //Step 2: Ask for the number of hours they worked.
+            Console.WriteLine("How many hours have you worked this week: ");
+            double Hours = Convert.ToDouble(Console.ReadLine());
+
+            //Step 3: Ask for their hourly pay.
+
+            Console.WriteLine("Enter your hourly wage: ");
+            double Wage = Convert.ToDouble(Console.ReadLine());
+
+            //Step 4: Multiply hours by pay.
+
+            Console.WriteLine("Hours * Pay: = " + (Hours * Wage));
+
+            //Step 5: Round the result to 2 decimal places.
+            double result = Hours * Wage;
+            double rounded = Math.Round(result, 1);
+
+            //Step 6: Display the result to the user.
+            Console.WriteLine("Employee " + ID + ", You have earned: " + rounded);
+
+        }
+    }
+}
